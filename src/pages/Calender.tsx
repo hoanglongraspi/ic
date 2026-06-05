c04ae0419a509d36159882ea67bd1635e5acd8ab
Long Cao <longcao@Longs-MacBook-Pro.local>
2026-06-05 16:15:32 -0400

    update

--- src/pages/Calender.tsx
+++ src/pages/Calender.tsx
@@ -14,7 +14,7 @@ export default function Calendar() {
     },
     { event: "Notification to Authors", date: "June 1, 2026" },
     { event: "Abstract Submission Deadline", date: "June 15, 2026" },
-    { event: "Early Registration Opens", date: "June 10, 2026" },
+    { event: "Early Registration Opens", date: "June 4, 2026" },
     { event: "Early Registration Deadline", date: "July 10, 2026" },
     { event: "ICIBM Conference", date: "August 2-5, 2026" },
   ];
